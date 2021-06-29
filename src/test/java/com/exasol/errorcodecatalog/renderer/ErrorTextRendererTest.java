348b51ca617e0f4f5d596b7d903c6cde04efd891
Jakob Braun <jakob.braun@posteo.de>
2021-06-29 03:24:05 -0300

    fixed test

--- src/test/java/com/exasol/errorcodecatalog/renderer/ErrorTextRendererTest.java
+++ src/test/java/com/exasol/errorcodecatalog/renderer/ErrorTextRendererTest.java
@@ -4,29 +4,35 @@ import static j2html.TagCreator.div;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import j2html.tags.DomContent;
 
 class ErrorTextRendererTest {
-    private static final ErrorTextRenderer RENDERER = new ErrorTextRenderer(new PlaceholderNumberProvider());
+    private ErrorTextRenderer renderer;
+
+    @BeforeEach
+    void beforeEach() {
+        this.renderer = new ErrorTextRenderer(new PlaceholderNumberProvider());
+    }
 
     @Test
     void testTextWithPlaceholder() {
-        final DomContent[] result = RENDERER.renderTextWithPlaceholders("My text with {{placeholder}}.");
+        final DomContent[] result = this.renderer.renderTextWithPlaceholders("My text with {{placeholder}}.");
         assertThat(div(result).render(),
                 equalTo("<div>My text with <span class=\"param\" nr=\"0\">{{placeholder}}</span>.</div>"));
     }
 
     @Test
     void testTextWithoutPlaceholder() {
-        final DomContent[] result = RENDERER.renderTextWithPlaceholders("My text without placeholder.");
+        final DomContent[] result = this.renderer.renderTextWithPlaceholders("My text without placeholder.");
         assertThat(div(result).render(), equalTo("<div>My text without placeholder.</div>"));
     }
 
     @Test
     void testTextWithTwoPlaceholders() {
-        final DomContent[] result = RENDERER.renderTextWithPlaceholders("{{A}}{{B}}");
+        final DomContent[] result = this.renderer.renderTextWithPlaceholders("{{A}}{{B}}");
         assertThat(div(result).render(), equalTo(
                 "<div><span class=\"param\" nr=\"0\">{{A}}</span><span class=\"param\" nr=\"1\">{{B}}</span></div>"));
     }
